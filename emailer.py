1453467dbc149abb3ad87d6903a7b105841ea52f
ge65jam <ge65jam@mytum.de>
2020-08-10 20:10:13 +0200

    Google test

--- emailer.py
+++ emailer.py
@@ -1,1 +1,23 @@
-import stmplib
+
+import os
+import smtplib
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = 'Grab dinner this weekend'
+    body = 'How about donner at 6 pm.'
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+
+
+
