68a6f5f95b73a192af4208fd47dea35589a67d00
ge65jam <ge65jam@mytum.de>
2020-08-10 21:24:10 +0200

    probando ambos

---
+++ emailerEllb.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = 'Grab dinner this weekend'
+    body = 'How about dinner at 6 pm.'
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
