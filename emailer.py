041d4fa762e0cc3456476f2b3f4478a2dbc08f5f
ge65jam <ge65jam@mytum.de>
2020-08-10 21:23:03 +0200

    back to gmail

--- emailer.py
+++ emailer.py
@@ -4,19 +4,18 @@ import smtplib
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-isa_user = 'ikervaire@ellb.com.pe'
-isa_pass = 'Peru2019'
 
-with smtplib.SMTP('mail.ellb.com.pe', 587) as smtp:
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
 
-    smtp.login(isa_user, isa_pass)
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
     subject = 'Grab dinner this weekend'
     body = 'How about dinner at 6 pm.'
 
     msg = f'Subject: {subject}\n\n{body}'
 
-    smtp.sendmail(isa_user, isa_user, msg)
+    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
